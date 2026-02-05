387f198404dd7798cfad7fe6807921fc51a849b0
Ambikkk <23311a0572@cse.sreenidhi.edu.in>
2026-02-05 15:19:18 +0530

    Initial commit

---
+++ cycle-2(program1)/script.js
@@ -0,0 +1,27 @@
+function generateRandomNumber() {
+  const randomNumber = Math.floor(Math.random() * 100) + 1;
+  const numDisplay = document.getElementById("randomNumber");
+
+  numDisplay.innerHTML = "";
+
+  const newPara = document.createElement("p");
+  newPara.innerHTML = "Random Number: " + randomNumber;
+
+  numDisplay.appendChild(newPara);
+}
+
+function addItem() {
+  const itemList = document.getElementById("itemList");
+  const newItem = document.createElement("li");
+
+  newItem.innerHTML = "Item " + (itemList.children.length + 1);
+  itemList.appendChild(newItem);
+}
+
+function removeItem() {
+  const itemList = document.getElementById("itemList");
+
+  if (itemList.children.length > 0) {
+    itemList.removeChild(itemList.lastChild);
+  }
+}
